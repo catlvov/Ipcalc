e721370a50ea54a63324b973f686453a2ea8fa5f
catlvov <kostalvov390@gmail.com>
2026-07-02 21:50:11 +0800

    Mask & prefix connection in progress

--- Ipcalc/main.cpp
+++ Ipcalc/main.cpp
@@ -12,43 +12,61 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR IpCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPARAM)
 {
+	HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+	HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+	HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+	DWORD dwIPaddress = 0;
+	DWORD dwIPmask = 0;
+	DWORD dwIPprefix = 0;
+	CHAR szIPprefix[32] = {};
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 		SendMessage(GetDlgItem(hwnd, IDC_SPIN_PREFIX), UDM_SETRANGE, 0, MAKEWORD(32, 0));
 		break;
 	case WM_COMMAND:
-	{
+	{/*
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
 		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 		DWORD dwIPaddress = 0;
 		DWORD dwIPmask = 0;
 		DWORD dwIPprefix = 0;
-		CHAR szIPprefix[8] = {};
+		CHAR szIPprefix[32] = {};*/
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
 			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
-			if (FIRST_IPADDRESS(dwIPaddress) < 128)SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 0, 0, 0));
+			/*if (FIRST_IPADDRESS(dwIPaddress) < 128)SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 0, 0, 0));
 			else if (FIRST_IPADDRESS(dwIPaddress) < 192)SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 255, 0, 0));
-			else if (FIRST_IPADDRESS(dwIPaddress) < 224)SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 255, 255, 0));
+			else if (FIRST_IPADDRESS(dwIPaddress) < 224)SendMessage(hIPmask, IPM_SETADDRESS, 0, MAKEIPADDRESS(255, 255, 255, 0));*/
+			if (FIRST_IPADDRESS(dwIPaddress) < 128)SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"8");
+			else if (FIRST_IPADDRESS(dwIPaddress) < 192)SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"16");
+			else if (FIRST_IPADDRESS(dwIPaddress) < 224)SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"24");
 			break;
 		case IDC_IPMASK:
-			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
-			for (dwIPprefix = 0; dwIPmask; dwIPprefix++, dwIPmask <<= 1)
+	/*		SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+			for (dwIPprefix = 0; dwIPmask; dwIPprefix++, dwIPmask <<= 1);
 			sprintf(szIPprefix, "%i", dwIPprefix);
-			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
+			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);*/
 			break;
 		case IDC_EDIT_PREFIX:
 		{
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
-				SendMessage(hEditPrefix, WM_GETTEXT, 3, (LPARAM)szIPprefix);
+				SendMessage(hEditPrefix, WM_GETTEXT, 31, (LPARAM)szIPprefix);
 				INT iPrefix = atoi(szIPprefix); //itoi() - ANSI/ASCII string to int
 				//ANSI/ASCII-String (NULL- Rerminated line) - строка, заканчивающа€с€ нулем
 				//ћассив элементов '', последним элементом которого €вл€етс€ "\0"
-				if (iPrefix > 32)SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"32");
+				if (iPrefix > 32)
+				{
+					SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"32");
+					iPrefix = 32;
+				}
+				dwIPmask = UINT_MAX; //все биты маски равны 1
+				for (INT i = 0; i < 32 - iPrefix; dwIPmask <<= 1, i++);
+				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
+
 			}
 		}
 		break;
@@ -58,6 +76,17 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPARAM)
 		}
 		break;
 	}
+	break;
+	case WM_NOTIFY:
+	{
+		if (wParam == IDC_IPMASK) {
+			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+			for (dwIPprefix = 0; dwIPmask; dwIPprefix++, dwIPmask <<= 1);
+			sprintf(szIPprefix, "%i", dwIPprefix);
+			SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szIPprefix);
+		}
+	}
+	break;
 	case WM_CLOSE: EndDialog(hwnd, 0);
 	}
 	return FALSE;
